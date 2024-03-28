cdf77a957fed14fcc5ca0d11b53f6d0f8cb9c873
Christopher R. Wood <chris@cyberethics.ca>
2024-03-27 21:17:03 -0400

    Add tests to verify `ValueError` is being raised

--- tests/test_degen.py
+++ tests/test_degen.py
@@ -1,9 +1,22 @@
+import pytest
+
 from deterministic_keygen import generate_phrase, derive_rsa_key_from_phrase
 
 
 def test_generate_phrase_returns_12_words() -> None:
     phrase = generate_phrase()
     assert len(phrase.split()) == 12
+
+
+@pytest.mark.parametrize("phrase, msg", [
+    ("abandon", "invalid number of words in phrase: 1"),
+    ("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon notaword", "invalid word in phrase"),
+    ("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon", "invalid checksum"),
+])
+def test_derive_rsa_key_from_phrase_raises_value_error(phrase, msg) -> None:
+    with pytest.raises(ValueError) as excinfo:
+        derive_rsa_key_from_phrase(phrase)
+    assert str(excinfo.value) == msg
 
 
 def test_derive_rsa_key_from_phrase() -> None:
