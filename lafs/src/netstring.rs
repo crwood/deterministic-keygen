71ace8236e96dc15cd9f30557909da19fb007a94
Christopher R. Wood <chris@cyberethics.ca>
2024-08-27 02:49:20 -0400

    Remove local "lafs" crate

--- lafs/src/netstring.rs
+++
@@ -1,9 +0,0 @@
-pub fn netstring(s: &[u8]) -> Vec<u8> {
-    format!("{}:{},", s.len(), std::str::from_utf8(s).unwrap()).into_bytes()
-}
-
-#[test]
-fn test_netstring() {
-    // Values from allmydata.test.test_netstring
-    assert_eq!(netstring(b"abc"), b"3:abc,");
-}
