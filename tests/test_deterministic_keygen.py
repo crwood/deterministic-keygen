4ccd77c0e80e2859ba85231fe0970574e9e94776
Christopher R. Wood <chris@cyberethics.ca>
2024-09-11 13:44:10 -0400

    Clean up tests

--- tests/test_deterministic_keygen.py
+++ tests/test_deterministic_keygen.py
@@ -3,17 +3,11 @@ from pathlib import Path
 import pytest
 import yaml
 
-from allmydata.util.base32 import b2a
-from allmydata.util.hashutil import (
-    _SHA256d_Hasher,  # XXX
-    ssk_pubkey_fingerprint_hash,
-    ssk_writekey_hash,
-)
-from cryptography.hazmat.primitives import serialization
 from deterministic_keygen import (
     generate_phrase,
     derive_rsa_key,
     derive_rsa_key_from_phrase,
+    # derive_lafs_mutable,
 )
 
 
@@ -47,42 +41,8 @@ def test_derive_rsa_key_from_phrase() -> None:
     assert key1 == key2
 
 
-@pytest.mark.parametrize("data, expected", [
-    (b"", b"lx3obytwcnm5gcucoucy4km7zqbycu2fix2vz5b6igmd6xkmsrla"),
-    (b"test", b"svgvusp5odm3rpg3gxjfejtyfgkx67xx7jwhj6eedg64l2bcbh2a"),
-])
-def test_sha256d(data, expected) -> None:
-    hasher = _SHA256d_Hasher()
-    hasher.update(data)
-    digest = hasher.digest()
-    assert b2a(digest) == expected
-
-
-def derive_lafs_mutable(private_key_pem: str, format: str) -> str:
-    private_key = serialization.load_pem_private_key(
-        private_key_pem.encode(),
-        password=None,
-    )
-    public_key = private_key.public_key()
-
-    privkey_der_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.DER,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption(),
-    )
-    pubkey_der_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.DER,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo,
-    )
-
-    writekey = b2a(ssk_writekey_hash(privkey_der_bytes)).decode()
-    fingerprint = b2a(ssk_pubkey_fingerprint_hash(pubkey_der_bytes)).decode()
-
-    return f"URI:{format}:{writekey}:{fingerprint}"
-
-
 def test_derive_lafs_mutable() -> None:
-    with open(Path(__file__).parent.parent / "lafs" / "tests" / "vectors" / "lafs.yaml") as f:
+    with open(Path(__file__).parent / "vectors" / "lafs.yaml") as f:
         data = yaml.safe_load(f)
     for vector in data["vector"]:
         kind = vector["format"]["kind"]
